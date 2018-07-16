18fa601bef35ddf61075cb0e3a664573c2f9bc86
Cierra Higgins <cierramhiggins@gmail.com>
2018-07-16 13:02:31 -0700

    added react component state

--- indecision_app/source/app.js
+++ indecision_app/source/app.js
@@ -55,6 +55,11 @@ class Action extends React.Component {
 
 
 class Options extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleRemoveAll = this.handleRemoveAll.bind(this);
+  }
+
   handleRemoveAll() {
     alert('removed all');
 
--- visibility_toggle.js
+++ visibility_toggle.js
@@ -4,8 +4,69 @@
 // Toggle some details you can now see
 // the button text changes from hide/show depending on details
 
-const showDetails = () => {
+class visibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+           this.state = {
+            visibility:false
+        };
+    }
+    handleToggleVisibility() {
+        this.setState((prevState) => {
+            return {
+                visibility: !prevState.visibility
+            };
+        });
+    }
+    render() {
+        return (
+            <div>
+                <h1>visibilityToggle</h1>
+                <button onClick={this.handleToggleVisibility}>
+                    {this.state.visibility ? 'Hide Details' : 'Show Details'}
+                </button>
+                {this.state.visibility && (
+                    <div>
+                      <p>Hey. Here are details</p>
+                    </div>
+            )}
+            </div>
+        );
+    }
+}
 
-};
+ReactDOM.render(<visibilityToggle />, document.getElementById('app'));
 
-<button onClick={showDetails}>Show Details</button>
+
+// let visibility = false;
+
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//     render();
+// };
+
+// const render = () => {
+//     const jsx = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleVisibility}>
+//                 {visibility ? 'Hide Details' : 'Show Details'}
+//             </button>
+//             {visibility && (
+//                 <div>
+//                     <p> details </p>
+//                 </div>
+//             )}
+//         </div>
+//     );
+
+//     ReactDOM.render(jsx, document.getElementById('app'));
+// };
+
+// render();
+// const showDetails = () => {
+
+// };
+
+// <button onClick={showDetails}>Show Details</button>
