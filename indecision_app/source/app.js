a3abb6697c7980547900fcfd2d68a8584ef99eb6
Cierra Higgins <cierramhiggins@gmail.com>
2018-07-24 10:34:58 -0700

    added lifecycle methods

--- indecision_app/source/app.js
+++ indecision_app/source/app.js
@@ -15,7 +15,28 @@ class IndecisionApp extends React.Component {
   }
 
   componentDidMount() {
-    console.log('componentDidMount!');
+    try {
+      const json = localStorage.getItem('options');
+      const options = JSON.parse(json);
+
+      if (options) {
+        this.setState(() => ({options}));
+      }
+    } catch (e) {
+
+    }
+
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.options.length !== this.state.options.length) {
+      const json = JSON.stringify(this.state.options);
+      localStorage.setItem('options', json);
+    }
+  }
+
+  componentWillUnmount() {
+    console.log('componentWillUnmount');
   }
 
   handleDeleteOptions() {
@@ -99,6 +120,7 @@ const Options = (props) => {
   return (
       <div>
       <button onClick={props.handleDeleteOptions}>Remove All</button>
+        {props.options.length === 0 && <p>Please add an option to get started</p>}
         {
             props.options.map((option) => (
               <Option key={option} optionText={option} handleDeleteOption={props.handleDeleteOption}/>
@@ -133,6 +155,10 @@ class AddOptions extends React.Component {
     const error = this.props.handleAddOption(option);
 
     this.setState(() => ({error}));
+
+    if (!error) {
+      e.target.elements.option.value = '';
+    }
   }
 
   render() {
