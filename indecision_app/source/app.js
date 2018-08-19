dc2320f57227871d2977014d72705d3e6d5f2493
Cierra Higgins <cierramhiggins@gmail.com>
2018-08-19 16:01:06 -0700

    continued webpack build set-up

--- indecision_app/source/app.js
+++ indecision_app/source/app.js
@@ -1,7 +1,7 @@
 
 import React from 'react'; 
 import ReactDOM from 'react-dom';
-import IndecisionApp from './components/IndecisionApp';
+import IndecisionApp from './components/indecisionApp';
 
 // stateless functional component
 // different from class based
