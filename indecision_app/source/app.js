2e6bb45a98cd5919dabf6564a9f42b2fe3791fd9
Cierra Higgins <cierramhiggins@gmail.com>
2018-08-19 17:38:50 -0700

    passing children to component

--- indecision_app/source/app.js
+++ indecision_app/source/app.js
@@ -3,6 +3,15 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/indecisionApp';
 
+const Layout = () => {
+  return (
+    <div>
+      <p>header</p>
+      <p>footer</p>
+    </div>
+  );
+}
+
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
 
 //old
